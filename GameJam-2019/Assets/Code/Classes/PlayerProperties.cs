7ee0434ccbf6184b424a1b62218be4031ee3dec7
pabloski300 <pabloski300@hotmail.com>
2020-01-31 19:23:09 +0100

    primer commit

---
+++ GameJam-2019/Assets/Code/Classes/PlayerProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Archivo de prueba.
+public class PlayerProperties 
+{
+    public float life;
+}
---
+++ GameJam-2019/Assets/Code/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Prueba.
+public class PlayerController : MonoBehaviour
+{
+    public PlayerProperties properties;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(properties.life);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
