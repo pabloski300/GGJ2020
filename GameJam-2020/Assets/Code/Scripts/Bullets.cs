f6dadad6019454763379d480695d1700dbde3c84
javihollow <javierbravo1998@outlook.com>
2020-02-01 15:12:19 +0100

    Controlador personaje

---
+++ GameJam-2020/Assets/Code/Scripts/Bullets.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    public int bullet_amount;
+    public bool destroyable;
+    public float time_to_live;
+    PlayerController controller;
+
+    public Bullets(int bullet_amount)
+    {
+        this.bullet_amount = bullet_amount;
+        destroyable = false;
+        time_to_live = 0;
+    }    
+
+    void Start()
+    {
+        controller = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        time_to_live -= Time.deltaTime;
+
+        if (time_to_live <= 0 && destroyable)
+        {
+            Debug.Log(controller);
+            controller.RemoveFromList(transform.gameObject);
+            Destroy(transform.gameObject);
+        }
+    }
+}
